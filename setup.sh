427eef452e17dbb51f04883917bed70f9f6259ea
Jim Hessin <jhessin@gmail.com>
2023-04-19 11:46:50 -0600

    Add echo message

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # TODO - Make sure these are going to the right places.
 
+echo You need sudo access to set up the keyboard
 sudo cp 10-keyboard.conf /etc/X11/xorg.conf.d/10-keyboard.conf
 sudo cp keymaps /etc/conf.d/keymaps
 sudo cp dvp.map.gz /usr/share/keymaps/i386/dvorak/dvp.map.gz
