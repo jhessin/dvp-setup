28b79526fb18bcf8232853280d59f435e305c98b
Jim Hessin <jhessin@gmail.com>
2023-04-19 11:31:00 -0600

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# TODO: clone reposotiry first
+
+sudo cp 10-keyboard.conf /etc/X11/xorg.conf.d/10-keyboard.conf
+sudo cp keymaps /etc/conf.d/keymaps
+sudo cp dvp.map.gz /usr/share/keymaps/i386/dvorak/dvp.map.gz
